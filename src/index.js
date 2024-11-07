d95c96eb95ff6648bed83de9e4791e3a7f0b5dbd
gribVolnushka <burgersqueen98@gmail.com>
2024-11-07 02:14:44 +0200

    added new variables for game

--- src/index.js
+++ src/index.js
@@ -1,42 +1,57 @@
-
-let move = prompt("Enter direction (UP, DOWN, LEFT, RIGHT) ");
+let movementPosition = prompt('Enter direction (UP, DOWN, LEFT, RIGHT)');
 const warningMessage = 'Wrong move, game over.';
-
-if (move.toUpperCase() === "LEFT") {
-  alert(move);
-  move = prompt(c);
-  if (move.toUpperCase() === 'LEFT'){
-    alert(move);
-    move = prompt("Enter direction (UP, DOWN, LEFT, RIGHT)");
-    if (move.toUpperCase() === 'LEFT'){
-      alert(move);
-      move = prompt("Enter direction (UP, DOWN, LEFT, RIGHT) ");
-      if (move.toUpperCase() === 'DOWN') {
-        alert(move);
-        move = prompt("Enter direction (UP, DOWN, LEFT, RIGHT) ");
-        if (move.toUpperCase() === 'DOWN') {
-          alert(move);
-          move = prompt("Enter direction (UP, DOWN, LEFT, RIGHT) ");
-          if (move.toUpperCase() === 'DOWN') {
-            alert(move);
-            move = prompt("Enter direction (UP, DOWN, LEFT, RIGHT) ");
-            if (move.toUpperCase() === 'RIGHT') {
-              alert(move);
-              move = prompt("Enter direction (UP, DOWN, LEFT, RIGHT) ");
-              if (move.toUpperCase() === 'RIGHT') {
-                alert(move);
-                move = prompt("Enter direction (UP, DOWN, LEFT, RIGHT) ");
-                if (move.toUpperCase() === 'RIGHT') {
-                  alert(move);
-                  move = prompt("Enter direction (UP, DOWN, LEFT, RIGHT) ");
-                  if (move.toUpperCase() === 'UP') {
-                    alert(move);
-                    move = prompt("Enter direction (UP, DOWN, LEFT, RIGHT) ");
-                    if (move.toUpperCase() === 'UP') {
-                      alert(move);
-                      move = prompt("Enter direction (UP, DOWN, LEFT, RIGHT) ");
-                      if (move.toUpperCase() === 'UP') {
-                        alert("Congratulations! You've reached the final destination.");
+const directionMoveToLeft = 'LEFT';
+const directionMoveToRight = 'RIGHT';
+const directionMoveToDown = 'Down';
+const directionMoveToUp = 'Up';
+const selectDirection = 'Enter direction (UP, DOWN, LEFT, RIGHT)';
+if (movementPosition.toUpperCase() === directionMoveToLeft) {
+  alert(movementPosition);
+  movementPosition = prompt(selectDirection);
+  if (movementPosition.toUpperCase() === directionMoveToLeft) {
+    alert(movementPosition);
+    movementPosition = prompt(selectDirection);
+    if (movementPosition.toUpperCase() === directionMoveToLeft) {
+      alert(movementPosition);
+      movementPosition = prompt(selectDirection);
+      if (movementPosition.toUpperCase() === directionMoveToDown) {
+        alert(movementPosition);
+        movementPosition = prompt(selectDirection);
+        if (movementPosition.toUpperCase() === directionMoveToDown) {
+          alert(movementPosition);
+          movementPosition = prompt(selectDirection);
+          if (movementPosition.toUpperCase() === directionMoveToDown) {
+            alert(movementPosition);
+            movementPosition = prompt(
+              selectDirection,
+            );
+            if (movementPosition.toUpperCase() === directionMoveToRight) {
+              alert(movementPosition);
+              movementPosition = prompt(
+                selectDirection,
+              );
+              if (movementPosition.toUpperCase() === directionMoveToRight) {
+                alert(movementPosition);
+                movementPosition = prompt(
+                  selectDirection,
+                );
+                if (movementPosition.toUpperCase() === directionMoveToRight) {
+                  alert(movementPosition);
+                  movementPosition = prompt(
+                    selectDirection,
+                  );
+                  if (movementPosition.toUpperCase() === directionMoveToUp) {
+                    alert(movementPosition);
+                    movementPosition = prompt(
+                      selectDirection
+                    );
+                    if (movementPosition.toUpperCase() === directionMoveToUp) {
+                      alert(movementPosition);
+                      movementPosition = prompt(
+                        selectDirection,
+                      );
+                      if (movementPosition.toUpperCase() === directionMoveToUp) {
+                        alert(`Congratulations! You've reached the final destination.`);
                       } else {
                         alert(warningMessage);
                       }
@@ -70,28 +85,32 @@ if (move.toUpperCase() === "LEFT") {
   } else {
     alert(warningMessage);
   }
-} else if (move.toUpperCase() === 'RIGHT'){
-  alert(move);
-  move = prompt("Enter direction (UP, DOWN, LEFT, RIGHT) ");
-  if (move.toUpperCase() === 'RIGHT') {
-    alert(move);
-    move = prompt("Enter direction (UP, DOWN, LEFT, RIGHT) ");
-    if (move.toUpperCase() === 'DOWN') {
-      alert(move);
-      move = prompt("Enter direction (UP, DOWN, LEFT, RIGHT) ");
-      if (move.toUpperCase() === 'LEFT') {
-        alert(move);
-        move = prompt("Enter direction (UP, DOWN, LEFT, RIGHT) ");
-        if (move.toUpperCase() === 'LEFT') {
-          alert(move);
-          move = prompt("Enter direction (UP, DOWN, LEFT, RIGHT) ");
-          if (move.toUpperCase() === 'LEFT') {
-            alert(move);
-            move = prompt("Enter direction (UP, DOWN, LEFT, RIGHT) ");
-            if (move.toUpperCase() === 'DOWN') {
-              alert(move);
-              move = prompt("Enter direction (UP, DOWN, LEFT, RIGHT) ");
-              if (move.toUpperCase() === 'RIGHT') {
+} else if (movementPosition.toUpperCase() === directionMoveToRight){
+  alert(movementPosition);
+  movementPosition = prompt(selectDirection);
+  if (movementPosition.toUpperCase() === directionMoveToRight) {
+    alert(movementPosition);
+    movementPosition = prompt(selectDirection);
+    if (movementPosition.toUpperCase() === directionMoveToUp) {
+      alert(movementPosition);
+      movementPosition = prompt(selectDirection);
+      if (movementPosition.toUpperCase() === directionMoveToLeft) {
+        alert(movementPosition);
+        movementPosition= prompt(selectDirection);
+        if (movementPosition.toUpperCase() === directionMoveToLeft) {
+          alert(movementPosition);
+          movementPosition = prompt(selectDirection);
+          if (movementPosition.toUpperCase() === directionMoveToLeft) {
+            alert(movementPosition);
+            movementPosition = prompt(
+              selectDirection,
+            );
+            if (movementPosition.toUpperCase() === directionMoveToDown) {
+              alert(movementPosition);
+              movementPosition = prompt(
+                selectDirection,
+              );
+              if (movementPosition.toUpperCase() === directionMoveToDown) {
                 alert("Congratulations! You've reached the final destination.")
               } else {
                 alert(warningMessage);
